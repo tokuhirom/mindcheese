608d68c1f0c3f9bdadca8b567d9a80996a978aa7
Tokuhiro Matsuno <tokuhirom@gmail.com>
2021-05-06 12:38:58 +0900

    Allow browser variables in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,8 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  env: {
+    browser: true,
+    node: true,
+  },
 };
