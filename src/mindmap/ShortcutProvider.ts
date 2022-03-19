bee758d680f2b6a10f417f1b54ac275915bcfe59
Tokuhiro Matsuno <tokuhirom@gmail.com>
2022-03-19 15:58:54 +0900

    remove duplicated

--- src/mindmap/ShortcutProvider.ts
+++ src/mindmap/ShortcutProvider.ts
@@ -13,20 +13,7 @@ export default class ShortcutProvider {
   constructor(
     jm: MindCheese,
     enable = true,
-    mappings: [number, string, (jm: MindCheese, e: Event) => boolean][] = [
-      [KeyModifier.NONE, "Delete", ShortcutHandlers.delete],
-      [KeyModifier.NONE, "Tab", ShortcutHandlers.addChild],
-      [KeyModifier.NONE, "Enter", ShortcutHandlers.addBrother],
-      [KeyModifier.CTRL, "Enter", ShortcutHandlers.editNode],
-      [KeyModifier.NONE, "Space", ShortcutHandlers.toggle],
-      [KeyModifier.SHIFT, "ArrowUp", ShortcutHandlers.moveUp],
-      [KeyModifier.SHIFT, "ArrowDown", ShortcutHandlers.moveDown],
-      [KeyModifier.NONE, "ArrowUp", ShortcutHandlers.up],
-      [KeyModifier.NONE, "ArrowDown", ShortcutHandlers.down],
-      [KeyModifier.NONE, "ArrowLeft", ShortcutHandlers.left],
-      [KeyModifier.NONE, "ArrowRight", ShortcutHandlers.right],
-      [KeyModifier.CTRL, "KeyZ", ShortcutHandlers.undo],
-    ]
+    mappings: [number, string, (jm: MindCheese, e: Event) => boolean][]
   ) {
     this.jm = jm;
     this.enable = enable;
