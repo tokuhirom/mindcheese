f8c87c129a566d921ecaaeafba8a073d18f22bff
tokuhirom <tokuhirom@gmail.com>
2022-03-21 13:27:56 +0000

    Deploying to gh-pages from @ tokuhirom/mindcheese@2a3a103dc877255db1ef816af3cf8a9c51c1fb17 🚀

--- browser.ts
+++ browser.ts
@@ -1,78 +1,64 @@
 import MindCheese from "../src/mindmap/MindCheese";
 import { DEMO_MARKDOWN, DEMO_NODE_TREE } from "./DemoData";
 
-export default MindCheese;
 console.log("Loaded browser.ts");
+
+function downloadText(filename: string, text: string) {
+  const element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
 
 function initDemo() {
   const container = document.getElementById("container");
   const mindCheese = new MindCheese(1, container);
   mindCheese.showNodeTree(DEMO_NODE_TREE);
 
-  // window.addEventListener("resize", () => resize());
-  // resize();
-  // function resize() {
-  //   const container = document.getElementById("container");
-  //   const menu = document.getElementById("menu");
-  //   const browserHeight = document.documentElement.clientHeight;
-  //   const bodyMarginTop = parseInt(
-  //     getComputedStyle(document.body).marginTop
-  //   );
-  //   const bodyMarginBottom = parseInt(
-  //     getComputedStyle(document.body).marginTop
-  //   );
-  //   container.style.height =
-  //     browserHeight -
-  //     menu.clientHeight -
-  //     bodyMarginTop -
-  //     bodyMarginBottom +
-  //     "px";
-  // }
-
-  document.getElementById("layout").addEventListener("click", () => {
-    console.log("layout.layout");
-    mindCheese.layout.layout();
-    return false;
-  });
-  document.getElementById("viewShow").addEventListener("click", () => {
-    console.log("viewShow");
-    mindCheese.view.show();
-    return false;
-  });
-  document.getElementById("expandSize").addEventListener("click", () => {
-    console.log("viewShow");
-    mindCheese.view.expandSize();
-    return false;
-  });
-  document.getElementById("dump_to_console").addEventListener("click", () => {
+  document.getElementById("download_json").addEventListener("click", () => {
     const data = mindCheese.getNodeTree();
-    console.log(data);
+    downloadText(
+      encodeURIComponent(mindCheese.mind.root.topic) + ".json",
+      JSON.stringify(data, null, 2)
+    );
     return false;
   });
-  document
-    .getElementById("dump_to_console_markdown")
-    .addEventListener("click", () => {
-      const data = mindCheese.getMarkdown();
-      console.log(data);
-      return false;
-    });
-  document.getElementById("resize").addEventListener("click", () => {
-    mindCheese.resize();
+  document.getElementById("download_markdown").addEventListener("click", () => {
+    const data = mindCheese.getMarkdown();
+    downloadText(encodeURIComponent(mindCheese.mind.root.topic) + ".md", data);
     return false;
   });
   document.getElementById("undo").addEventListener("click", () => {
     mindCheese.undo();
     return false;
   });
-  document.getElementById("load_markdown").addEventListener("click", () => {
-    mindCheese.showMarkdown(DEMO_MARKDOWN);
-    return false;
-  });
-  document.getElementById("dark").addEventListener("click", () => {
-    mindCheese.setTheme("dark");
+
+  if (process.env.BUILD == "development") {
+    document.getElementById("load_markdown").addEventListener("click", () => {
+      mindCheese.showMarkdown(DEMO_MARKDOWN);
+      return false;
+    });
+  } else {
+    document.getElementById("navItemDebug").style.display = "none";
+  }
+
+  let themeMode = true;
+  document.getElementById("toggle_theme").addEventListener("click", () => {
+    mindCheese.setTheme(themeMode ? "dark" : "primary");
+    themeMode = !themeMode;
     return false;
   });
 }
 
 // @ts-ignore
 window.initDemo = initDemo;
+export default initDemo;
